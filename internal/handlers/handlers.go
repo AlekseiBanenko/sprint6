fc1eb0ea34271d4c8fe820117a181266efad75f4
Aleksei Banenko <79374270054@yandex.ru>
2026-02-06 00:40:39 +0300

    test handlers2

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
 
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
 )
@@ -33,40 +32,6 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	contentType := r.Header.Get("Content-Type")
-	if strings.Contains(contentType, "multipart/form-data") {
-		err := r.ParseMultipartForm(10 << 20)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		file, _, err := r.FormFile("file")
-		if err != nil {
-			http.Error(w, "no such file", http.StatusInternalServerError)
-			return
-		}
-		defer file.Close()
-
-		data, err := io.ReadAll(file)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		result, err := service.AutoConvert(string(data))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.Write([]byte(fmt.Sprintf(`
-<!DOCTYPE html>
-<html><head><title>Result</title></head><body><h1>Result:</h1><pre>%s</pre><a href="/">← Back</a></body></html>`, result)))
-		return
-	}
-
 	data, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -79,6 +44,16 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Write([]byte(result))
+	contentType := r.Header.Get("Content-Type")
+	if contentType == "" || contentType == "text/plain" {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte(result))
+	} else {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write([]byte(fmt.Sprintf(`
+<!DOCTYPE html>
+<html><head><title>Result</title></head><body>
+<h1>Result:</h1><pre>%s</pre><a href="/">← Back</a>
+</body></html>`, result)))
+	}
 }

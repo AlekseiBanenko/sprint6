9257f9bf074adcc81e2fee6c8fb9d70a0ba1a925
Aleksei Banenko <79374270054@yandex.ru>
2026-02-08 12:52:21 +0300

    replay5

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -40,7 +40,7 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	file, header, err := r.FormFile("file")
+	file, _, err := r.FormFile("file")
 	if err != nil {
 		log.Println("Error retrieving file:", err)
 		w.WriteHeader(http.StatusInternalServerError)
